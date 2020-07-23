d8c8bd7b4f98d2a0993bfea979701d9ffaeee963
ajg7 <ajgebara@gmail.com>
2020-07-23 15:54:57 -0600

    finished tasks 5 & 6. Still need to finish task 2

--- index.js
+++ index.js
@@ -84,13 +84,11 @@ class Car {
   fill(gallons) {
     this.tank += gallons;
   }
-  drive(distance, fuel) {
+  drive(distance) {
     this.odometer += distance;
     this.tank -= (distance/this.milesPerGallon);
 
-    if(fuel === 0) {
-      return `I ran out of fuel at ${this.odometer}`;
-    }
+    
   }
 }
 
@@ -161,8 +159,22 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(obj){
+    super(obj)
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+  listSubjects() {
+    return `Loving ${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}`
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 }
 
 /*
@@ -178,8 +190,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(obj){
+    super(obj)
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
